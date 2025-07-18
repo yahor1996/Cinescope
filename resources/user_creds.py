0df482dc793b4d7430f92f34a6c37de77c5fe726
yahor1996 <EgorGromovYur@yandex.ru>
2025-07-18 20:27:08 +0300

    modified user_creds security

--- resources/user_creds.py
+++ resources/user_creds.py
@@ -1,4 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class SuperAdminCreds:
+    USERNAME = os.getenv('SUPER_ADMIN_USERNAME')
+    PASSWORD = os.getenv('SUPER_ADMIN_PASSWORD')
+
+
+"""
 class SuperAdminCreds:
     USERNAME = 'api1@gmail.com'
     PASSWORD = 'asdqwe123Q'
+"""
 
