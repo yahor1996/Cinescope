c936109b5c3dd0ed3569f882b490ad00ad4df996
yahor1996 <EgorGromovYur@yandex.ru>
2025-07-26 12:23:50 +0300

    added example pydantic

--- models/registration_user_data.py
+++ models/registration_user_data.py
@@ -1,3 +1,5 @@
+import pytest
+from typing import Optional
 from pydantic import BaseModel
 from Cinescope.constants.roles import Roles
 from Cinescope.utils.data_generator import DataGenerator
@@ -9,10 +11,13 @@ class UserData(BaseModel):
     fullName: str
     password: str
     passwordRepeat: str
-    roles: list[str]
+    roles: list[Roles]
+    banned: Optional[bool] = None
+    verified: Optional[bool] = None
 
 
-def get_user_data():
+@pytest.fixture(scope="function")
+def test_user():
     random_email = DataGenerator.generate_random_email()
     random_name = DataGenerator.generate_random_name()
     random_password = DataGenerator.generate_random_password()
@@ -22,10 +27,55 @@ def get_user_data():
         "fullName": random_name,
         "password": random_password,
         "passwordRepeat": random_password,
-        "roles": [Roles.USER.value]
+        "roles": [Roles.USER]
     }
 
 
-def test_user_data():
-    user = UserData(**get_user_data())  # Проверяем возможность конвертации данных и соответствия типов данных с помощью Pydantic
-    logger.info(f"{user.email=} {user.fullName=} {user.password=} {user.passwordRepeat=} {user.roles=}")  # а также возможность удобного взаимодействия
+@pytest.fixture(scope="function")
+def creation_user_data(test_user):
+    """
+    Фикстура для создания пользователя.
+    """
+    updated_data = test_user.copy()
+    updated_data.update({
+        "verified": True,
+        "banned": False
+    })
+    return updated_data
+
+
+
+def test_user_data(test_user, creation_user_data):
+    # Проверяем возможность конвертации данных и соответствия типов данных с помощью Pydantic
+    test_user = UserData(**test_user)
+
+    # Проверяем возможность удобного взаимодействия
+    logger.info(
+        f"{test_user.email=} "
+        f"{test_user.fullName=} "
+        f"{test_user.password=} "
+        f"{test_user.passwordRepeat=} "
+        f"{test_user.roles=} "
+        f"{test_user.banned=} "
+        f"{test_user.verified=}"
+    )
+
+    json_test_user = test_user.model_dump_json(exclude_unset=True)
+    logger.info(json_test_user)
+
+    # Проверяем возможность конвертации данных и соответствия типов данных с помощью Pydantic
+    creation_user = UserData(**creation_user_data)
+
+    # Проверяем возможность удобного взаимодействия
+    logger.info(
+        f"{creation_user.email=} "
+        f"{creation_user.fullName=} "
+        f"{creation_user.password=} "
+        f"{creation_user.passwordRepeat=} "
+        f"{creation_user.roles=} "
+        f"{creation_user.banned=} "
+        f"{creation_user.verified=}"
+    )
+
+    json_creation_user = creation_user.model_dump_json()
+    logger.info(json_creation_user)
