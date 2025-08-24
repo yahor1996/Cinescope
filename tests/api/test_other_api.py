2db8f80e4a80af54bea2004bb9a36db290a0ee67
yahor1996 <EgorGromovYur@yandex.ru>
2025-08-24 11:57:15 +0300

    add allure steps fo test

--- tests/api/test_other_api.py
+++ tests/api/test_other_api.py
@@ -1,3 +1,4 @@
+import allure
 from Cinescope.conftest.conftest import *
 from datetime import datetime, timedelta
 from pytz import timezone
@@ -49,66 +50,83 @@ class TestMoviesDB:
         assert movies_from_db.count() == 0, "Фильм не был удален из базы!"
 
 
+@allure.epic("Тестирование транзакций")
+@allure.feature("Тестирование транзакций между счетами")
+class TestAccountTransactionTemplate:
 
+    @allure.story("Корректность перевода денег между двумя счетами")
+    @allure.description("""
+    Этот тест проверяет корректность перевода денег между двумя счетами.
+    Шаги:
+    1. Создание двух счетов: Stan и Bob.
+    2. Перевод 200 единиц от Stan к Bob.
+    3. Проверка изменения балансов.
+    4. Очистка тестовых данных.
+    """)
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.label("qa_name", "Ivan Petrovich")
+    @allure.title("Тест перевода денег между счетами 200 рублей")
     def test_accounts_transaction_template(self, db_session: Session):
         # ====================================================================== Подготовка к тесту
-        # Создаем новые записи в базе данных (чтоб точно быть уверенными что в базе присутствуют данные для тестирования)
+        with allure.step("Создание тестовых данных в базе данных: счета Stan и Bob"):
+            stan = AccountTransactionTemplate(user=f"Stan_{DataGenerator.generate_random_name()}", balance=1000)
+            bob = AccountTransactionTemplate(user=f"Bob_{DataGenerator.generate_random_name()}", balance=500)
 
-        stan = AccountTransactionTemplate(user=f"Stan_{DataGenerator.generate_random_name()}", balance=1000)
-        bob = AccountTransactionTemplate(user=f"Bob_{DataGenerator.generate_random_name()}", balance=500)
+            # Добавляем записи в сессию
+            db_session.add_all([stan, bob])
+            # Фиксируем изменения в базе данных
+            db_session.commit()
 
-        # Добавляем записи в сессию
-        db_session.add_all([stan, bob])
-        # Фиксируем изменения в базе данных
-        db_session.commit()
-
+        @allure.step("Функция перевода денег: transfer_money")
+        @allure.description("""
+                    функция выполняющая транзакцию, имитация вызова функции 
+                    на стороне тестируемого сервиса
+                    и вызывая метод transfer_money, 
+                    мы как будто бы делаем запрос в api_manager.movies_api.transfer_money
+                    """)
         def transfer_money(session, from_account, to_account, amount):
-            # пример функции выполняющей транзакцию
-            # представим что она написана на стороне тестируемого сервиса
-            # и вызывая метод transfer_money, мы какбудтобы делем запрос в api_manager.movies_api.transfer_money
             """
-            Переводит деньги с одного счета на другой.
             :param session: Сессия SQLAlchemy.
             :param from_account_id: ID счета, с которого списываются деньги.
             :param to_account_id: ID счета, на который зачисляются деньги.
             :param amount: Сумма перевода.
             """
-            # Получаем счета
-            from_account = session.query(AccountTransactionTemplate).filter_by(user=from_account).one()
-            to_account = session.query(AccountTransactionTemplate).filter_by(user=to_account).one()
+            with allure.step(" Получаем счета"):
+                from_account = session.query(AccountTransactionTemplate).filter_by(user=from_account).one()
+                to_account = session.query(AccountTransactionTemplate).filter_by(user=to_account).one()
 
-            # Проверяем, что на счете достаточно средств
-            if from_account.balance < amount:
-                raise ValueError("Недостаточно средств на счете")
+            with allure.step("Проверяем, что на счете достаточно средств"):
+                if from_account.balance < amount:
+                    raise ValueError("Недостаточно средств на счете")
 
-            # Выполняем перевод
-            from_account.balance -= amount
-            to_account.balance += amount
+            with allure.step("Выполняем перевод"):
+                from_account.balance -= amount
+                to_account.balance += amount
 
-            # Сохраняем изменения
-            session.commit()
+            with allure.step("Сохраняем изменения"):
+                session.commit()
 
         # ====================================================================== Тест
-        # Проверяем начальные балансы
-        assert stan.balance == 1000
-        assert bob.balance == 500
+        with allure.step("Проверяем начальные балансы"):
+            assert stan.balance == 1000
+            assert bob.balance == 500
 
         try:
-            # Выполняем перевод 200 единиц от stan к bob
-            transfer_money(db_session, from_account=stan.user, to_account=bob.user, amount=200)
+            with allure.step("Выполняем перевод 200 единиц от stan к bob"):
+                transfer_money(db_session, from_account=stan.user, to_account=bob.user, amount=200)
 
-            # Проверяем, что балансы изменились
-            assert stan.balance == 800
-            assert bob.balance == 700
+            with allure.step("Проверяем, что балансы изменились"):
+                assert stan.balance == 800
+                assert bob.balance == 700
 
         except Exception as e:
-            # Если произошла ошибка, откатываем транзакцию
-            db_session.rollback()  # откат всех введеных нами изменений
+            with allure.step("Если произошла ошибка, откатываем транзакцию"):
+                db_session.rollback()
             pytest.fail(f"Ошибка при переводе денег: {e}")
 
         finally:
-            # Удаляем данные для тестирования из базы
-            db_session.delete(stan)
-            db_session.delete(bob)
-            # Фиксируем изменения в базе данных
-            db_session.commit()
+            with allure.step("Удаляем данные для тестирования из базы"):
+                db_session.delete(stan)
+                db_session.delete(bob)
+            with allure.step("Фиксируем изменения в базе данных"):
+                db_session.commit()
