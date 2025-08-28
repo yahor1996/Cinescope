218ddadc02aa28fb64ae3711c8700adc5550f5bb
yahor1996 <EgorGromovYur@yandex.ru>
2025-08-28 20:32:31 +0300

    added test with soft asserts

--- tests/api/test_auth_api.py
+++ tests/api/test_auth_api.py
@@ -1,5 +1,7 @@
+from pytest_check import check
+import allure
+from datetime import datetime
 from sqlalchemy.orm import sessionmaker, Session
-
 from conftest.conftest import *
 from Cinescope.db_requester.models import UserDBModel
 from Cinescope.resources.user_creds import SuperAdminCreds
@@ -74,3 +76,38 @@ class TestAuthAPI:
         user_from_db = users_from_db.first()
         # Можем осуществить проверку всех полей в базе данных например Email
         assert user_from_db.email == test_user.email, "Email не совпадает"
+
+    @allure.title("Тест регистрации пользователя с помощью Mock")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.label("qa_name", "Ivan Petrovich")
+    def test_register_user_mock(self, api_manager: ApiManager, test_user: TestUser, mocker):
+        with allure.step(" Мокаем метод register_user в auth_api"):
+            mock_response = RegisterUserResponse(  # Фиктивный ответ
+                id="id",
+                email="email@email.com",
+                fullName="fullName",
+                verified=True,
+                banned=False,
+                roles=[Roles.SUPER_ADMIN],
+                createdAt=str(datetime.now())
+            )
+
+            mocker.patch.object(
+                api_manager.auth_api,  # Объект, который нужно замокать
+                'register_user',  # Метод, который нужно замокать
+                return_value=mock_response  # Фиктивный ответ
+            )
+
+        with allure.step("Вызываем метод, который должен быть замокан"):
+            register_user_response = api_manager.auth_api.register_user(test_user)
+
+        with allure.step("Проверяем, что ответ соответствует ожидаемому"):
+            with allure.step("Проверка поля персональных данных"):  # обратите внимание на вложенность allure.step
+                with check:
+                    # Строка ниже выдаст исклющение и но выполнение теста продолжится
+                    check.equal(register_user_response.fullName, "INCORRECT_NAME", "НЕСОВПАДЕНИЕ fullName")
+                    check.equal(register_user_response.email, mock_response.email)
+
+            with allure.step("Проверка поля banned"):
+                with check("Проверка поля banned"):  # можно использовать вместо allure.step
+                    check.equal(register_user_response.banned, mock_response.banned)
